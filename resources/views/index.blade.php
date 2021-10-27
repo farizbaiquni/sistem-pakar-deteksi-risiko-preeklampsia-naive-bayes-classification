67714c842b1767fc87c7d918820d8b7d7b2869c7
unknown <farizbaiquni@gmail.com>
2021-10-27 15:41:00 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Home | Pre-eklampsia</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b7e4c7;">
+        <div class="container">
+            <a class="navbar-brand" href="#" style="font-weight: bolder">Sistem pakar deteksi risiko pre-eklampsia</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                </ul>
+                <form class="d-flex">
+                    <a class="btn btn-success" href="/daftar" role="button" style="margin-right: 20px;">Daftar</a>
+                    <a class="btn btn-success" href="#" role="button">Masuk</a>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container" style="display: flex; justify-content: center; margin-top: 70px;">
+        <div class="div" style="display: flex; flex-direction: column;">
+            <h1 style="margin: auto;">Selamat Datang</h1>
+            <h2 style="margin: auto;">Silahkan Daftar atau Masuk ke dalam Sistem</h2>
+            <div style="margin: auto">
+                <img src="/images/ibu_hamil.jpg" style="width: 500px; height: auto;" alt="">
+            </div>
+        </div>
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
